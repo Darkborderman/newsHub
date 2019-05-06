9f473a3659a335732ad23f1a632d5b263122e467
darkborderman <reastw1234@gmail.com>
2019-05-06 12:40:10 +0800

    Add project overview, setup linting

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-let express = require('express');
-let Parser = require('rss-parser');
-let parser = new Parser();
-let request = require('request');
+const express = require('express');
+const Parser = require('rss-parser');
 
-var app = express();
+const parser = new Parser();
+const app = express();
 
 (async () => {
-    let feed = await parser.parseURL('https://www.bnext.com.tw/rss');
-    console.log(feed.title);
-    console.log(feed.description);
-    console.log(feed.link);
-    for(item in feed.items){
-        console.log(feed.items[item].title);
-        console.log(feed.items[item].contentSnippet);
-    }
+  const feed = await parser.parseURL('https://www.bnext.com.tw/rss');
+  console.log(feed.title);
+  console.log(feed.description);
+  console.log(feed.link);
+  feed.items.forEach((element) => {
+    // console.log(element);
+    console.log(element.title);
+    console.log(element.contentSnippet);
+  });
 })();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
 });
-
